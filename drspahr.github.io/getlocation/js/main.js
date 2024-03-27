c1710583c86688e42675a655c5454f7517a86171
drspahr <spahrdg@gmail.com>
2024-03-27 15:07:36 -0500

    Testing geolocations and maps

---
+++ drspahr.github.io/getlocation/js/main.js
@@ -0,0 +1,40 @@
+const x = document.getElementById('demo');
+
+const buttonGPS = document.querySelector('.gpsBtn');
+buttonGPS.addEventListener('click', getLocation);
+
+function getLocation() {
+    try {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } catch {
+        x.innerHTML = err;
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = 'Latitude: ' + position.coords.latitude + '<br>Longitude ' + position.coords.longitude;
+}
+
+let map;
+async function initMap() {
+    // The location of user
+    const position = {lat: 38.10042527593918, lng: -97.91837067673404};
+    const {Map} = await google.maps.importLibrary('maps');
+    const {AdvancedMarkerElement} = await google.maps.importLibrary('marker');
+    // The map
+    map = new Map(document.getElementById('map'), {
+        zoom: 4,
+        center: position,
+        mapId: "Demo_Map_ID",
+    });
+    //The marker
+    const marker = new AdvancedMarkerElement({
+        map: map,
+        position: position,
+        title: "Home",
+    });
+}
+
+initMap();
+
+// const apiKey = "AIzaSyCd8eRTAaNNAXnBsVJ8ADdk0aVGEnedVcs"
